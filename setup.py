522f0b6394285d8e222732593fb1f7dedbcc4444
callistusystan <callistusystan@gmail.com>
2017-11-27 13:19:18 +1100

    Update requirements and setup

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
 setup(name='Chord123',
-      version='0.1',
+      version='0.3',
       description='A command line script to translate a .docx of number-system chords to a specified key',
       author='Callistus Tan',
       author_email='callistusystan@example.com',
+      packages=['chord123'],
+      keywords=['music', 'translate', 'chords', 'numbers'],
       url='https://github.com/callistusystan/Chord123',
+      download_url='https://github.com/callistusystan/Chord123/archive/0.3.tar.gz',
       license='MIT',
       entry_points = {
         "console_scripts": ['chord123 = chord123.chord123:main']
-        },
-      packages=['chord123'],
+      },
+      install_requires=[
+        'python-docx',
+      ],
       zip_safe=False)
